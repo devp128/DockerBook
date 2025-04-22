e5dc0ac01fd4429727bdbc370f4109b357caa36e
Dev Prasad <devpatra2000@gmail.com>
2025-04-22 14:43:54 +0530

    origin updated

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['https://book-store-sable-pi.vercel.app', 'http://localhost:4173'],
+  origin: ['https://docker-book.vercel.app', 'http://localhost:4173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
